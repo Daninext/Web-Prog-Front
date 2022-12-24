cba04b09784467b30085f9e9a35db50b0a106840
Daninext <47050120+Daninext@users.noreply.github.com>
2022-12-24 09:15:30 +0300

    fix: time calc, link color

--- scripts/visualEff.js
+++ scripts/visualEff.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', event => colorButton())
 
 function colorButton() {
-    let file = document.location.href.split("/").at(-1);
+    let file = document.location.pathname;
     let navigation = document.getElementsByClassName("nav-item");
     for(let i = 0; i < navigation.length; ++i) {
-        if (navigation.item(i).attributes.getNamedItem("href").value == file) {
+        if (file.includes(navigation.item(i).attributes.getNamedItem("href").value)) {
             navigation.item(i).setAttribute("style", "color: azure");
         }
     }
